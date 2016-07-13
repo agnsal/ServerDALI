922d6220618e4ddef4cb45f27ea0731400d78e92
Agnese Salutari <agneses92@hotmail.it>
2016-07-13 16:54:42 +0200

    Update VDati.php

--- View/VDati.php
+++ View/VDati.php
@@ -11,11 +11,11 @@
  * @author Agnese Salutari
  */
 class VDati extends View{
-    public function mostraPagina($l_c,$f_c,$t,$s_c){
-        $this->assign('felice', $f_c);
-        $this->assign('lavoro', $l_c);
+    public function mostraPagina($l,$f,$t,$s){
+        $this->assign('felice', $f);
+        $this->assign('lavoro', $l);
         $this->assign('tempo', $t);
-        $this->assign('saggezza', $s_c);
+        $this->assign('saggezza', $s);
         $this->display('dati.tpl');
     }
 }
